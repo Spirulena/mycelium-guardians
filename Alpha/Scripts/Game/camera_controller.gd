991f5c7691601e1dd5fa68367a430da67844f89a
FelineX <sara210804@gmail.com>
2025-10-13 19:07:26 +0100

    implement new drag and move with middle mouse button with new crosshair cursors
    implemented a common RTS middle mouse button and drag mechanic with additional new cursor sprites and change mechanic

--- Alpha/Scripts/Game/camera_controller.gd
+++ Alpha/Scripts/Game/camera_controller.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
-@export var move_speed : float = 150
+#@export var move_speed : float = 150
+@export var pan_speed : float = .25
 @export var zoom_amount : float = 1.25
 
 var default_position: Vector2 = Vector2(-79, -139)
@@ -11,22 +12,50 @@ var is_resetting := false
 
 var playerUnit
 
+var dragging = false
+var last_mouse_position: Vector2
+
+var move_cursor = preload("res://Alpha/2D assets/Cursors/move.png")
+var default_cursor = preload("res://Alpha/2D assets/Cursors/arrow.png")
+
 func _ready():
+	Input.set_custom_mouse_cursor(default_cursor, Input.CURSOR_ARROW, Vector2(0, 0) )
+
 	playerUnit = get_tree().get_first_node_in_group("Player")
 
 func _process(delta):
-	moveCamera(delta)
+	#moveCamera(delta)
+	dragCamera(delta)
 	zoomCamera(delta)
 	handleFollow(delta)
 
-func moveCamera(delta):
-	var input = Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
-	var zoom_mod = 4 - zoom.x
-
-	if input != Vector2.ZERO and is_following:
-		is_following = false
+func dragCamera(delta):
+	if Input.is_action_just_pressed("camera_drag"):
+		Input.set_custom_mouse_cursor(move_cursor, Input.CURSOR_ARROW, Vector2(0, 0))
 		
-	global_position += input * delta * move_speed * zoom_mod
+		dragging = true
+		last_mouse_position = get_viewport().get_mouse_position()
+	elif Input.is_action_just_released("camera_drag"):
+		dragging = false
+		Input.set_custom_mouse_cursor(default_cursor, Input.CURSOR_ARROW, Vector2(0, 0))
+
+	if dragging:
+		var mouse_pos = get_viewport().get_mouse_position()
+		var mouse_delta = mouse_pos - last_mouse_position
+
+		var zoom_mod = 4 - zoom.x
+
+		position -= mouse_delta * pan_speed * zoom_mod
+		last_mouse_position = mouse_pos
+
+#func moveCamera(delta):
+	#var input = Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
+	#var zoom_mod = 4 - zoom.x
+#
+	#if input != Vector2.ZERO and is_following:
+		#is_following = false
+		#
+	#global_position += input * delta * move_speed * zoom_mod
 
 func zoomCamera(delta):
 	var z = zoom.x
