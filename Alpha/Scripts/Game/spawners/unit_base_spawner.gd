eb7acb6c5b808fd24eff65b4fcba490549593a7e
FelineX <sara210804@gmail.com>
2025-10-22 22:15:10 +0100

    set up for unit base spawner

---
+++ Alpha/Scripts/Game/spawners/unit_base_spawner.gd
@@ -0,0 +1,20 @@
+extends Node2D
+class_name UnitSpawner
+
+var num_units : int = 0 #how many units this spawner has
+var total_units : int = 0 #how many units are out in the scene
+
+# reference to ground layer and obstacle layer from grid
+# reference to spawner layer
+
+# reference to base_unit of type player 
+
+# reference to number of units label above spawner
+
+# process function
+# updates the label above the spawner to the number of units the spawner has
+# always awaiting a button press function
+
+# button function
+
+# function that spawns units with a button press
