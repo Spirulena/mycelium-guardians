36cc325bf9b8ca5b7cfc5074d96ca65f230e458d
FelineX <sara210804@gmail.com>
2026-01-14 16:05:05 +0000

    Update selection info display format
    Updated the displayed text format for selected tiles to show coordinates, layer, and resource in a clearer multiline format.

--- Alpha/Scripts/Presenters/SelectionInfoPresenter.gd
+++ Alpha/Scripts/Presenters/SelectionInfoPresenter.gd
@@ -8,12 +8,10 @@ func on_selection_changed(prev: TileObject, curr: TileObject):
 		var coords = curr.get_coords()
 		var building_at = curr.get_building_type()
 		var ruin_at = curr.get_ruin()
-		text = "Tile at (%d, %d): Building type %d, ruin type %s" % [coords.x, coords.y, building_at, ruin_at]
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+		
+		text = "Coords (%d, %d)\nLayer: %d\nResource: %s" % [
+		coords.x,
+		coords.y,
+		building_at,
+		ruin_at
+		]
